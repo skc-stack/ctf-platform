80a8125f64327907462965aa1d78a564809c0ddb
root <root@eschool-573301.vm.kh.edu.tw>
2026-09-20 11:45:02 +0800

    Revert "fix(student-task): show Target VM IP instead of entrypoint URL"
    This reverts commit 52be0630afe63b8a8a23afd3ddd38382bef407b6.

--- src/Repositories/TaskSessionRepository.php
+++ src/Repositories/TaskSessionRepository.php
@@ -33,11 +33,9 @@ final class TaskSessionRepository
                     c.title AS challenge_title,
                     c.slug AS challenge_slug,
                     c.version AS challenge_version,
-                    c.uuid AS challenge_uuid,
-                    d.last_ip AS target_ip
+                    c.uuid AS challenge_uuid
              FROM task_sessions t
              JOIN challenges c ON c.id = t.challenge_id
-             LEFT JOIN devices d ON d.id = t.device_id
              WHERE t.id = :id',
             [':id' => $id]
         );
